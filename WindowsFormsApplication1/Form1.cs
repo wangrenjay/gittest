60711efcc0804da77334d62503bd0a5c56b66d45
wangrenjay <wangrenjay@163.com>
2014-09-10 08:42:34 +0800

    第一次

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            LibGit2Sharp.Handlers.
+            //LibGit2Sharp.Handlers.
         }
     }
 }
